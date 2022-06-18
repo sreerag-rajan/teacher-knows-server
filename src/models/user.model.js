0ca01477109e94a991f7d0d5d0e244ff3e6d4d1e
Sreerag <sreerag.rajan@hotmail.com>
2022-06-18 19:16:26 +0530

    slight changes to field names in user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-    first_name: {type: String, required:true},
-    last_name: {type: String, required:true},
+    firstName: {type: String, required:true},
+    lastName: {type: String, required:true},
     email: {type: String, required:true, unique:true},
     password : {type: String, required:true}
 },
