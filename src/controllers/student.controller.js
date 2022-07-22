e8dc2206ceb159aa94559329b8c525f985d031fe
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-07-22 19:49:52 +0530

    added basic student controller

---
+++ src/controllers/student.controller.js
@@ -0,0 +1,79 @@
+const express = require('express');
+
+const Student = require('../models/student.model');
+
+const router = express.Router();
+
+//Helper Functions
+const getAllStudents = (classId, search={}) => {
+  return new Promise((resolve, reject) => {
+    Student.find({classId}).lean().exec()
+      .then((students)=> resolve(students))
+      .catch(er => reject(er))
+
+  })
+}
+
+//Routes
+router.post('/', async (req, res) => {
+  try{
+    await Student.create(req.body)
+    const students = await getAllStudents(req.body.classId);
+    return res.status(200).json(students);
+
+  }
+  catch(er){
+    console.error('ERROR ::: create student route ::: ', er);
+    return res.status(500).json({error:er});
+  }
+})
+
+router.get('/', async (req, res) => {
+  try{
+    const {search} = req.params;
+    const students = await getAllStudents(req.body.classId, search);
+    return res.status(200).json(students);
+  }
+  catch(er){
+    console.error('ERROR ::: get all students route ::: ', er);
+    return res.status(500).json({error:er});
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try{
+    const student = await Student.findById(req.params.id).lean().exec();
+    return res.status(200).json(student);
+  }
+  catch(er){
+    console.error('ERROR ::: get a student route ::: ', er);
+    return res.status(500).json({error:er});
+  }
+})
+
+router.patch('/:id', async (req, res) => {
+  try{
+    const student = await Student.findByIdAndUpdate(req.params.id, req.body, {new:true}).lean().exec();
+    return res.status(200).json(student);
+
+  }
+  catch(er){
+    console.error('ERROR ::: update student route ::: ', er);
+    return res.status(500).json({error:er});
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try{
+    let info = await Student.findByIdAndDelete(req.params.id);
+    const students = getAllStudents(info.classId);
+    return res.status(200).json(students);
+
+  }
+  catch(er){
+    console.error('ERROR ::: delete students route ::: ', er);
+    return res.status(500).json({error:er});
+  }
+})
+
+module.exports = router
--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const authController = require("./controllers/auth.controller");
 const tokenController = require('./controllers/token.controller');
 const subjectController = require('./controllers/subject.controller');
 const classesController = require('./controllers/classes.controller');
+const studentController = require('./controllers/student.controller');
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.use("/auth", authController);
 app.use('/token', tokenController);
 app.use('/subject', authenticate, subjectController);
 app.use('/classes', authenticate, classesController);
+app.use('/student', authenticate, studentController);
 
 
 //connecting to server and database;
