15c1ddf16ff4d8fb68d679013f2aa9177505fbde
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-07-27 21:33:46 +0530

    minor changes to classController

--- src/controllers/classes.controller.js
+++ src/controllers/classes.controller.js
@@ -7,7 +7,7 @@ const router = express.Router();
 //Helper functions
 const getAllClasses = async (user) => {
   return new Promise((resolve, reject)=>{
-    Classes.find({user}).sort({'grade': 1, 'section': 1}).lean().exec()
+    Classes.find({user}).populate('subjects', ['name', '_id']).sort({'grade': 1, 'section': 1}).lean().exec()
       .then((classes) => resolve(classes))
       .catch(er => reject(er));
   })
--- src/models/class.models.js
+++ src/models/class.models.js
@@ -5,8 +5,8 @@ const mongoose = require("mongoose");
 const classSchema = new mongoose.Schema({
     grade : {type: Number, required:true},
     section : {type: String, required:true},
-    subject: [{type: mongoose.Schema.Types.ObjectId, ref:"subject"}],
-    numOfStudents : {type: Number, default:0, required:false},
+    subjects: [{type: mongoose.Schema.Types.ObjectId, ref:"subject"}],
+    numberOfStudents : {type: Number, default:0, required:false},
     user : {type: mongoose.Schema.Types.ObjectId, ref:"user", required:true}
 },{
     timestamps:true,
